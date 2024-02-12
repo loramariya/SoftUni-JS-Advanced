c04234477e9e938f359a5037920dcabe134821e9
loramariya <loramaria.raynova@gmail.com>
2024-02-12 13:37:07 +0200

    Update

--- 05.DOM_Manipulation_and_Events _Lab/07. Shopping-Cart/solution.js
+++ 05.DOM_Manipulation_and_Events _Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-   //TODO...
+   const addButtons = document.querySelectorAll(".add-product");
+   const checkoutButton = document.querySelector(".checkout");
+   const textareaElement = document.querySelector("textarea");
+
+   let products = [];
+   let totalPrice = 0;
+
+   for (const button of addButtons) {
+      button.addEventListener("click", addProduct);
+   }
+
+   checkoutButton.addEventListener("click", checkout);
+
+   function addProduct(event) {
+      const productElement = event.target.parentElement.parentElement;
+      const productTitle = productElement.querySelector(".product-title");
+      const productPrice = productElement.querySelector(".product-line-price");
+
+      printMessage(productTitle, productPrice);
+
+      if (!products.includes(productTitle.textContent)) {
+         products.push(productTitle.textContent);
+      }
+
+      totalPrice += Number(productPrice.textContent);
+   }
+
+   function printMessage(productTitle, productPrice) {
+      textareaElement.textContent +=
+         `Added ${productTitle.textContent} for ${productPrice.textContent} to the cart.\n`;
+   }
+
+   function checkout(event) {
+      textareaElement.textContent +=
+         `You bought ${products.join(', ')} for ${totalPrice.toFixed(2)}.`;
+      disableButtons();
+   }
+
+   function disableButtons() {
+      let buttons = Array.from(document.querySelectorAll("button"));
+      buttons.forEach(button => button.disabled = true);
+   }
 }
