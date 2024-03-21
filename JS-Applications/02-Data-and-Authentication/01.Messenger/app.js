0567f91e2a181073e1a4473ae8c444692ee28903
loramariya <loramaria.raynova@gmail.com>
2024-03-21 14:04:49 +0200

    Upload solutions

--- JS-Applications/02-Data-and-Authentication/01.Messenger/app.js
+++ JS-Applications/02-Data-and-Authentication/01.Messenger/app.js
@@ -1,5 +1,36 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("refresh").addEventListener("click", onLoadMsg);
+    document.getElementById("submit").addEventListener("click", onSubmit);
+    const url = "http://localhost:3030/jsonstore/messenger"
+
+    async function onSubmit(e) {
+        let nameRef = document.querySelector("input[name='author']");
+        let textRef = document.querySelector("input[name='content']");
+        let name = nameRef.value;
+        let text = textRef.value;
+        let data = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ author: name, content: text })
+        }
+
+        await fetch(url, data);
+        nameRef.value = "";
+        textRef.value = "";
+    }
+
+    async function onLoadMsg(e) {
+        let textAreaRef = document.getElementById("messages");
+        textAreaRef.value = "";
+        let response = await fetch(url);
+        let data = await response.json();
+        Object.values(data).forEach(rect => {
+            textAreaRef.value += `${rect.author}: ${rect.content}\n`;
+        })
+        textAreaRef.value = textAreaRef.value.trim();
+    }
 }
 
 attachEvents();
--- JS-Applications/02-Data-and-Authentication/03.Students/app.js
+++ JS-Applications/02-Data-and-Authentication/03.Students/app.js
@@ -1,1 +1,46 @@
-console.log('TODO...');
+const url = 'http://localhost:3030/jsonstore/collections/students';
+const formRef = document.querySelector('#form');
+const tbodyRef = document.querySelector('tbody');
+window.addEventListener('load', loadStudents);
+formRef.addEventListener('submit', submitHandler);
+
+async function loadStudents(){
+    tbodyRef.innerHTML = "";
+    const requestAll = await fetch(url);
+    const data = await requestAll.json();
+    Object.values(data).forEach(item => {
+        let inner = '<tr>';
+        inner += `<td>${item.firstName}</td>`;
+        inner += `<td>${item.lastName}</td>`;
+        inner += `<td>${item.facultyNumber}</td>`;
+        inner += `<td>${Number(item.grade).toFixed(2)}</td>`;
+        tbodyRef.innerHTML += inner + '</tr>';
+    });
+}
+
+async function submitHandler(e){
+    e.preventDefault();
+    const first = formRef.elements['firstName'].value;
+    const last = formRef.elements['lastName'].value;
+    const facultyNumber = formRef.elements['facultyNumber'].value;
+    const grade = formRef.elements['grade'].value;
+
+    if (!first || !last || !facultyNumber || !grade){
+        return;
+    }
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            firstName: first,
+            lastName: last,
+            facultyNumber: facultyNumber,
+            grade: grade
+        })
+    });
+    formRef.reset();
+    loadStudents();
+}
